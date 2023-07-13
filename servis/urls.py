df40fbdbc33c6817f09f0a833cda100ee19b3db1
MelihOZKAS <melihozkass@gmail.com>
2023-07-13 21:45:04 +0300

    feat: Servis aktariliyor.

---
+++ servis/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path("", views.home, name = "home"),
+   # path("logout/", views.logout, name='logout'),
+   # path("bayi/", views.basvuru, name = "bayi"),
+   # path("panel/", views.panel, name = "panel"),
+   # path("mnt/", views.evrak, name = "mnt"),
+   # path("kontorlu-yeni-hat/", views.kontorluYeni, name = "kontorlu-yeni-hat"),
+   # path("faturali-yeni-hat/", views.FaturaliYeni, name = "faturali-yeni-hat"),
+   # path("sekebe-ici-gecis/", views.sebekeicigecis, name = "sekebe-ici-gecis"),
+   # path("internet-basvuru/", views.internet, name = "internet-basvuru"),
+   # path("evrak-gir-pass/", views.evrakpass, name = "evrak-gir-pass"),
+   # path("kontor-yukle/", views.kontor, name = "kontor-yukle"),
+   # path("alt-yapi-sorgula/", views.altYapi, name = "alt-yapi-sorgula"),
+]
