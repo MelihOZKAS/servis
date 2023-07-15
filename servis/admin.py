e660271b949a152b0a850b9fc17deb6c9b2fdb67
MelihOZKAS <melihozkass@gmail.com>
2023-07-15 16:37:59 +0300

    feat: Link güncellemeleri v8

--- servis/admin.py
+++ servis/admin.py
@@ -2,9 +2,17 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Kayit
-
+from .models import Kayit
 
 
 class KayitAdmin(admin.ModelAdmin):
     list_display = ('isim','numara','konu','aciklama','durumu')
     list_editable = ('durumu')
+
+
+
+admin.site.register(Kayit, KayitAdmin)
+
+
+
+
